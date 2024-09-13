d5c9b9ed076f09869a88bb2aa409ca99ff36269d
hgb926 <hgb926@naver.com>
2024-09-13 21:38:57 +0900

    0913 프로그래머스 알고리즘

--- programmer/수 조작하기1.js
+++ programmer/수 조작하기1.js
@@ -1,22 +1,21 @@
 function solution(n, control) {
-    var answer = 0;
     for (let i = 0; i < control.length; i++) {
         switch (control[i]) {
             case "w":
-                answer++;
+                n++;
                 break;
             case "s":
-                answer--;
+                n--;
                 break;
             case "d":
-                answer += 10;
+                n += 10;
                 break;
             case "a":
-                answer -= 10;
+                n -= 10;
                 break;
         }
     }
-    return answer;
+    return n;
 }
 let number = solution(0, "wsdawsdassw");
 console.log(number)
