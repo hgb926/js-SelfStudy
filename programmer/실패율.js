53903955de6beac5e453c154b2782e19c6694486
hgb926 <hgb926@naver.com>
2024-11-18 22:57:37 +0900

    1118 코테 알고리즘

--- programmer/실패율.js
+++ programmer/실패율.js
@@ -1,15 +1,38 @@
 const solution = (N, stages) => {
     // 객체 매핑
-    let map = {};
-    stages.sort((a, b) => a - b);
+    let stageMap = {};
+    stages.sort((a, b) => b - a);
+    console.log(stages)
     for (let i = 0; i < stages.length; i++) {
-        if (map[stages[i]]) {
-            map[stages[i]]++
+        if (stageMap[stages[i]]) {
+            stageMap[stages[i]]++
         } else {
-            map[stages[i]] = 1;
+            stageMap[stages[i]] = 1;
         }
     }
+    console.log(stageMap)
 
+    // 역 누적 로직
+    let result = {};
+    let keys = Object.keys(stageMap).map(Number).sort((a, b) => a - b);
+
+    for (let i = 0; i < keys.length; i++) {
+        let currentKey = keys[i]
+        let sum = 0;
+        for (let j = i; j < keys.length; j++) {
+            sum += stageMap[keys[j]];
+        }
+        result[currentKey] = sum;
+    }
+    console.log(result)
+
+    for (const stage in stageMap) {
+        // stage = key
+        // stageMap[stage]; = value
+        // 현재 순회하고있는 key보다 높은 key의 value들을
+        // 다 더해서 key와 다시 매핑해야 한다.
+
+    }
 
 
 }
